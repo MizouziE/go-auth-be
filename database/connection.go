5bcef0347afb1120d333a80e3aef0afcff3c60c6
MizouziE <s.anglin@live.co.uk>
2022-12-31 17:30:37 +0100

    auth back end

---
+++ database/connection.go
@@ -0,0 +1,22 @@
+package database
+
+import (
+	"github.com/MizouziE/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	connection, err := gorm.Open(mysql.Open("developer:developer@/developer"), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to database")
+	}
+
+	DB = connection
+
+	connection.AutoMigrate(&models.User{})
+
+}
